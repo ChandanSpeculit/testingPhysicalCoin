0b1e99f404691b5ab1dbb1cf6aedd35d8b94cb4e
ChandanSpeculit <chadandan007.behera@speculit.com>
2024-10-01 16:46:15 +0530

    chandan

--- src/Pages/Products/Address.jsx
+++ src/Pages/Products/Address.jsx
@@ -219,7 +219,7 @@ function Address() {
                 alert("Payment Done");
                 // Use a slight delay to ensure the alert has time to be shown before redirecting
                 setTimeout(() => {
-                    window.location.href = '/myOrders';
+                    navigate('/myOrders');
                 }, 3000);
             })
             .catch((error) => {
